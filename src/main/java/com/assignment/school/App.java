46c4188c00d3476449645d12f0e0afbc1d7b6a05
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-01-25 17:50:05 +0530

    Update App.java

--- src/main/java/com/assignment/school/App.java
+++ src/main/java/com/assignment/school/App.java
@@ -2,7 +2,7 @@ package com.assignment.school;
 import java.util.Scanner;
 public class App 
 {
-    public static void main(String args[]) {
+    public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter name:");
