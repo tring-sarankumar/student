e4a3d8888802d3f8d6415d8be59f06550034755f
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-01-30 16:00:45 +0530

    Update Student.java

--- src/main/java/com/assignment/school/Student.java
+++ src/main/java/com/assignment/school/Student.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 public class Student 
 {
-    public static void main(String args[]) {
+    public static void main(String []args) {
     	Logger log =  Logger.getLogger("CreditCard");
     	
 
