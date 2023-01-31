280c5db667b556817ee6b5cf114688f0163f430d
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-01-31 14:23:10 +0530

    commit

--- src/main/java/com/assignment/school/Student.java
+++ src/main/java/com/assignment/school/Student.java
@@ -1,41 +1,33 @@
 package com.assignment.school;
 import java.util.Scanner;
 import java.util.logging.Logger;
-public class Student 
-{
-    public static void main(String []args) {
-    	Logger log =  Logger.getLogger("CreditCard");
-    	
-
+public class Student {
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger("Student");
 
         Scanner sc = new Scanner(System.in);
-        try {
+
         log.info("Enter name:");
-        String name =sc.nextLine();
+        String name = sc.nextLine();
 
         log.info("Enter grade:");
-        char grade=sc.next().charAt(0);
-        
+        char grade = sc.next().charAt(0);
+
         log.info("Enter GPA:");
-        float gpa=sc.nextFloat();
-        
+        float gpa = sc.nextFloat();
+
         StudentLogic obj = new StudentLogic(name, grade, gpa);
-        String prof=obj.profile();
+        String prof = obj.profile();
         log.info(prof);
-        
+
         log.info("Do You Want To Update GPA: yes/no");
-        String updategpa= sc.next();
-        
-        if(updategpa.equalsIgnoreCase("yes")){
+        String updategpa = sc.next();
+
+        if (updategpa.equalsIgnoreCase("yes")) {
             log.info("Enter New GPA:");
-            Float newgpa =sc.nextFloat();
+            Float newgpa = sc.nextFloat();
             obj.update(newgpa);
-            log.info("Successfully Updated");}
-        }catch (Exception e) {
-        	log.info("There Was an Exception :"+e);
-        }
-        finally {
-        	sc.close();
+            log.info("Successfully Updated");
         }
     }
 }
